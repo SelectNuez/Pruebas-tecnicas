eecb0448027e0cbb18bf251cee6542fdef22d178
Jorge Martin <selectnuez@gmail.com>
2024-05-21 20:44:43 +0200

    Funcionalidad basica

--- 01-add-items-react/src/App.tsx
+++ 01-add-items-react/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-
+type ItemId = `${string}-${string}-${string}-${string}-${string}`;
 interface Item {
-  id: `${string}-${string}-${string}-${string}-${string}`,
-  timestamp: number,
-  text: string
+  id: ItemId;
+  timestamp: number;
+  text: string;
 }
 
 const INITIAL_ITEMS: Item[] = [
@@ -23,18 +23,45 @@ const INITIAL_ITEMS: Item[] = [
     id: crypto.randomUUID(),
     timestamp: Date.now(),
     text: "Documentales",
-  }
-]
+  },
+];
 
 function App() {
-  const [items, setItems] = useState(INITIAL_ITEMS)
+  const [items, setItems] = useState(INITIAL_ITEMS);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { elements } = event.currentTarget;
+    const input = elements.namedItem("item");
+    const isInput = input instanceof HTMLInputElement;
+    if (!isInput || input == null) return;
+
+    const newItem: Item = {
+      id: crypto.randomUUID(),
+      timestamp: Date.now(),
+      text: input.value,
+    };
+
+    setItems((prevItems) => {
+      return [...prevItems, newItem];
+    });
+    input.value = "";
+  };
+
+  const createHandleRemoveItem = (id: ItemId) => () => {
+    setItems((prevItems) => {
+      return prevItems.filter((currentItem) => currentItem.id !== id);
+    });
+  };
+
   return (
     <main>
       <aside>
         <h1>Prueba tecnica React</h1>
         <h2>Añadir y eliminar elementos de una lista</h2>
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="">
             Elemento a introducir
             <input name="item" required type="text" placeholder="Elemento" />
@@ -44,17 +71,24 @@ function App() {
       </aside>
       <section>
         <h2>Lista de elementos</h2>
-        <ul>
-          {
-            items.map(item => {
-              return(
+        {items.length === 0 ? (
+          <p>
+            <strong>No hay elementos en la lista</strong>
+          </p>
+        ) : (
+          <ul>
+            {items.map((item) => {
+              return (
                 <li key={item.id}>
-                  { item.text }
+                  {item.text}{" "}
+                  <button onClick={createHandleRemoveItem(item.id)}>
+                    Eliminar elementos
+                  </button>
                 </li>
-              )
-            })
-          }
-        </ul>
+              );
+            })}
+          </ul>
+        )}
       </section>
     </main>
   );
